ceaa7e42468068b3fe127e58083fadc060b0b0d9
Tyler Yahn <codingalias@gmail.com>
2022-12-16 09:28:33 -0800

    Add exporter to logger

--- otlpr.go
+++ otlpr.go
@@ -17,22 +17,26 @@
 package otlpr
 
 import (
+	"context"
+
 	"github.com/MrAlias/otlpr/internal"
 	"github.com/go-logr/logr"
+	collpb "go.opentelemetry.io/proto/otlp/collector/logs/v1"
+	lpb "go.opentelemetry.io/proto/otlp/logs/v1"
 	"google.golang.org/grpc"
 )
 
 func New(conn *grpc.ClientConn) logr.Logger {
 	// FIXME: handle nil conn.
 	l := &logger{
-		conn:      conn,
+		exp:       newExporter(conn),
 		formatter: internal.NewFormatter(internal.Options{}),
 	}
 	return logr.New(l)
 }
 
 type logger struct {
-	conn *grpc.ClientConn
+	exp *exporter
 
 	formatter internal.Formatter
 	level     int
@@ -46,7 +50,47 @@ func (l *logger) Enabled(level int) bool {
 	return level >= l.level
 }
 
-func (l *logger) Info(level int, msg string, keysAndValues ...interface{})
-func (l *logger) Error(err error, msg string, keysAndValues ...interface{})
-func (l *logger) WithValues(keysAndValues ...interface{}) logr.LogSink
-func (l *logger) WithName(name string) logr.LogSink
+func (l *logger) Info(level int, msg string, keysAndValues ...interface{}) {
+	l.exp.enqueue(l.formatter.FormatInfo(level, msg, keysAndValues))
+}
+
+func (l *logger) Error(err error, msg string, keysAndValues ...interface{}) {
+	l.exp.enqueue(l.formatter.FormatError(err, msg, keysAndValues))
+}
+
+func (l *logger) WithValues(keysAndValues ...interface{}) logr.LogSink {
+	l.formatter.AddValues(keysAndValues)
+	return l
+}
+
+func (l *logger) WithName(name string) logr.LogSink {
+	l.formatter.AddName(name)
+	return l
+}
+
+type exporter struct {
+	client collpb.LogsServiceClient
+}
+
+func newExporter(conn *grpc.ClientConn) *exporter {
+	return &exporter{client: collpb.NewLogsServiceClient(conn)}
+}
+
+func (e *exporter) enqueue(msg *lpb.LogRecord) {
+	// TODO: handle batching.
+	_, _ = e.client.Export(context.Background(), &collpb.ExportLogsServiceRequest{
+		ResourceLogs: []*lpb.ResourceLogs{
+			{
+				ScopeLogs: []*lpb.ScopeLogs{
+					{
+						LogRecords: []*lpb.LogRecord{
+							msg,
+						},
+					},
+				},
+			},
+		},
+	})
+	// TODO: handle partial success response.
+	// TODO: handle returned error (log it?).
+}
